e43f14e4b08b920d607682eddeb9b77378c6cc7a
Nickolas Markus <113360684+nikolau96@users.noreply.github.com>
2023-12-23 19:43:10 -0300

    Add files via upload

---
+++ LPI-exercicios/Exercicio7.java
@@ -0,0 +1,24 @@
+public class Exercicio7{
+    int numero1 = 3;
+    int numero2 = 3;
+    
+    public void soma(){
+        int resultado = numero1 + numero2;
+        System.out.println(resultado);
+    }
+
+    public void subtracao(){
+        int resultado = numero1 - numero2;
+        System.out.println(resultado);
+    }
+
+    public void multiplicacao(){
+        int resultado = numero1 * numero2;
+        System.out.println(resultado);
+    }
+
+    public void divisao(){
+        int resultado = numero1 / numero2;
+        System.out.println(resultado);
+    }
+}
