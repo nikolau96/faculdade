badfa714ee7cfbd7fc55c9bfa1488df4d6cfd96b
Nickolas Markus <113360684+nikolau96@users.noreply.github.com>
2023-12-23 23:02:46 -0300

    Add files via upload

---
+++ LPI-exercicios/Triangular.java
@@ -0,0 +1,14 @@
+public class Triangular {
+    int a;
+    int b;
+    int c;
+
+    public void numeroTriangular(){
+        if(a*b*c==120){
+            System.out.println("É um número triangular");
+        }
+        else{
+            System.out.println("Não é um número triangular");
+        }
+    }
+}
